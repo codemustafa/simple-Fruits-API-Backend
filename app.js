4b721dfdaf8a1182553c37d7f19d5d0604f520f4
codemustafa <15mohamedm@gmail.com>
2026-05-06 12:38:14 +0100

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //How to run the program
 
 const express = require('express')
+const cors = require('cors')
 const app = express();
 const fruits = require("./routes/fruits")
 
@@ -8,6 +9,7 @@ app.get('/', (req, res) => {
     res.send("Hello from the fruits api")
 })
 
+app.use(cors())
 app.use(express.json())
 app.use('/fruits', fruits) // http://localhost:3000/fruits
 
